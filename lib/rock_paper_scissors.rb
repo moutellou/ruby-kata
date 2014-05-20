1fb27e6d63320002aad9fe2f5d4d948c14f31c48
moutellou <moutellou@gmail.com>
2014-05-19 23:05:53 -0300

    Rock-scissors-paper part

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,7 +4,18 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+   strategy1 = player1[1]
+   strategy2 = player2[1]
+   raise NoSuchStrategyError, "Strategy must be one of R,P,S" unless  
+	 (strategy1.match(/[PRS]/) && strategy2.match(/[PRS]/))
+  	 return player1 if strategy1 == strategy2
+   if (strategy1 == 'P' && strategy2 == 'R') || 
+      (strategy1 == 'R' && strategy2 == 'S') ||
+      (strategy1 == 'S' && strategy2 == 'P') 
+        return player1
+   else
+	return player2
+   end     
   end
 
   def self.tournament_winner(tournament)
