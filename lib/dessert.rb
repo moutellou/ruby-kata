5cf1398666e56287db6fb083ab42a9b5ce6396f2
moutellou <moutellou@gmail.com>
2014-05-19 22:29:20 -0300

    Dessert part

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,25 @@
 class Dessert
+attr_accessor :name
+attr_accessor :calories
   def initialize(name, calories)
-    # your code here
+   @name=name
+   @calories = calories
   end
   def healthy?
-    # your code here
+    @calories < 200
   end
   def delicious?
-    # your code here
+    true
   end
 end
 
 class JellyBean < Dessert
+attr_accessor :flavor
   def initialize(flavor)
-    # your code here
+    super("vanilla jelly bean",5)
+    @flavor=flavor     
+  end
+  def delicious?
+    @flavor != 'licorice'
   end
 end
